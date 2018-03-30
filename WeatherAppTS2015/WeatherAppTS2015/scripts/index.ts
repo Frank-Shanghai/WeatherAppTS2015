ef29f453cf30a55e97a41ce7459676acae7eb79f
Frank <null.reference@hotmail.com>
2018-03-30 22:12:33 +0800

    Added Jquery, Jquey.Mobile, Jquery.TypeScript.Definition package, no package for JQuery.Mobile available. Updated UI and CSS.

--- WeatherAppTS2015/WeatherAppTS2015/scripts/index.ts
+++ WeatherAppTS2015/WeatherAppTS2015/scripts/index.ts
@@ -16,11 +16,6 @@ module WeatherAppTS2015 {
             document.addEventListener('resume', onResume, false);
 
             // TODO: Cordova has been loaded. Perform any initialization that requires Cordova here.
-            var parentElement = document.getElementById('deviceready');
-            var listeningElement = parentElement.querySelector('.listening');
-            var receivedElement = parentElement.querySelector('.received');
-            listeningElement.setAttribute('style', 'display:none;');
-            receivedElement.setAttribute('style', 'display:block;');
         }
 
         function onPause() {
